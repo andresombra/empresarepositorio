9c606950f6fb8528ac4f58be2cf1086244b0ba24
andresombra <andresombra.net@gmail.com>
2025-06-05 12:18:57 -0300

    Atualizacao

---
+++ Empresa/Empresa.Api/Controllers/TempController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empresa.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TempController : Controller
+    {
+        [HttpGet("public")]
+        [AllowAnonymous] // Permite acesso público a este endpoint específico
+        public IActionResult Teste()
+        {
+            return Ok("This is a public endpoint.");
+        }
+    }
+}
